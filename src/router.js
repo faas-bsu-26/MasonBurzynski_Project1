2da7cb9b71bc4c4403b6170f7548b5d3d30955c3
MasonBurzy <mason.burzynski@bsu.edu>
2026-03-20 12:21:34 -0400

    updating router.js

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import About from './components/pages/About.vue'
 import AddIdea from './components/pages/AddIdea.vue'
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 })
 
