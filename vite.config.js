b57b38dd9451df5dbc0916a28d1ed664471b2f33
MasonBurzy <mason.burzynski@bsu.edu>
2026-03-20 11:44:36 -0400

    changing base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://faas-bsu-26.github.io/MasonBurzynski_Project1/"
+  base: "/MasonBurzynski_Project1/"
 })
